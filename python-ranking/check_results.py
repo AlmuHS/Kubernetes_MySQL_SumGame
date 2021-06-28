ac7690d0e6e037e0e4b7df2468f35e2be68c8fe7
Almudena Garcia <liberamenso10000@gmail.com>
2021-06-28 16:45:40 +0200

    move ranking to port 80

--- python-ranking/check_results.py
+++ python-ranking/check_results.py
@@ -32,4 +32,4 @@ def main():
         return render_template("ranking.html", data=results, headers=headers)
         
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5001)
+        app.run(host='0.0.0.0', port=80)
